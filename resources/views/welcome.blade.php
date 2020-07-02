97ce413b4957e579e88c44c0b68f784cbe7c9ad0
Sonali Hirave <hiravesonali@gmail.com>
2020-07-02 22:35:54 +0200

    added user

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -113,6 +113,29 @@
                                 <button type="button" class="text-sm bg-red-500 hover:bg-red-700 text-white py-1 px-2 rounded focus:outline-none focus:shadow-outline">Delete</button>
                             </td>
                         </tr>
+                        <tr class="border-b hover:bg-orange-100">
+                            <td class="p-3 px-5"><input type="text" value="John Doe" class="bg-transparent"></td>
+                            <td class="p-3 px-5"><input type="text" value="john@admin.com" class="bg-transparent"></td>
+                            <td class="p-3 px-5"><input type="text" value="$45" class="bg-transparent"></td>
+                            <td class="p-3 px-5"><input type="text" value="Adidas" class="bg-transparent"></td>
+                            <td class="p-3 px-5">
+                                <select value="Category" class="bg-transparent">
+                                    <option value="user">Women</option>
+                                    <option value="admin">Man</option>
+                                    <option value="admin">Kids</option>
+                                </select>
+                            </td>
+                            <td class="p-3 px-5">
+                                <select value="status" class="bg-transparent">
+                                    <option value="user">available</option>
+                                    <option value="admin">not available</option>
+                                </select>
+                            </td>
+                            <td class="p-3 px-5 flex justify-end">
+                                <button type="button" class="mr-3 text-sm bg-blue-500 hover:bg-blue-700 text-white py-1 px-2 rounded focus:outline-none focus:shadow-outline">Save</button>
+                                <button type="button" class="text-sm bg-red-500 hover:bg-red-700 text-white py-1 px-2 rounded focus:outline-none focus:shadow-outline">Delete</button>
+                            </td>
+                        </tr>
                     </tbody>
                 </table>
             </div>
